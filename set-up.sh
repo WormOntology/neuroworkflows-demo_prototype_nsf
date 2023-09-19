69a1713f8a746b91ccf12b8c27ced5b71ed031a5
Daniel Xenes <daniel.xenes@jhuapl.edu>
2023-09-19 11:48:53 -0400

    allows set up to skip nextflow reinstall

--- set-up.sh
+++ set-up.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Step 1: Download Nextflow
-curl -s https://get.nextflow.io | bash
+# Check if Nextflow is installed
+if ! command -v nextflow &> /dev/null
+then
+    # If Nextflow is not installed, download and install it
+    curl -s https://get.nextflow.io | bash
+    # Add Nextflow to the PATH environment variable
+    export PATH=$PATH:$(pwd)
+else
+    echo "Nextflow is already installed"
+fi
 
-# Step 2: Add Nextflow to the PATH environment variable
-export PATH=$PATH:$(pwd)
-
-# Step 3: Run Docker Compose
+# Run Docker Compose
 docker compose up -d
